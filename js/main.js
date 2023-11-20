5a88f9223ecdfcb0d72f8b1c6b57538e38e19be1
Omar-Stone <151367335+Omar-Stone@users.noreply.github.com>
2023-11-20 21:53:59 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /* ===================================================================
- *  Knox 1.0.0 - Main JS
+ *  Stoneood 1.0.0 - Main JS
  *
  *
  * ------------------------------------------------------------------- */
@@ -15,7 +15,7 @@
         // Countdown Timer Final Date
         finalDate : 'December 12, 2023 00:00:00',
         // MailChimp URL
-        mailChimpURL : 'https://facebook.us1.list-manage.com/subscribe/post?u=1abf75f6981256963a47d197a&amp;id=37c6d8f4d6' 
+     //   mailChimpURL : 'https://facebook.us1.list-manage.com/subscribe/post?u=1abf75f6981256963a47d197a&amp;id=37c6d8f4d6' 
 
     };
 

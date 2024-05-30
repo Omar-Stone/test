1a9f4db0da139b6539c099538680f163fc4ec9c8
Omar-Stone <omarxsheet@gmail.com>
2024-05-30 16:00:06 +0400

    small updates

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@
     const cfg = {
 
         // Countdown Timer Final Date
-        finalDate : 'December 12, 2023 00:00:00',
+        finalDate : 'May 12, 2024 00:00:00',
         // MailChimp URL
      //   mailChimpURL : 'https://facebook.us1.list-manage.com/subscribe/post?u=1abf75f6981256963a47d197a&amp;id=37c6d8f4d6' 
 
